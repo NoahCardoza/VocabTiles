5ce14c3333894e59fc06b3a11a9a2b50b4053da9
NoahCardoza <noahcardoza@gmail.com>
2020-10-05 14:27:22 -0700

    added todo so we don't forget to remove the global vuesax import later

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import Vuesax from 'vuesax'
 
+/* 
+  TODO: while global imports are fine for a hackathon, we should make sure
+  to import only the components we need later on. There might be a smart way
+  to configure webpack to do this but I can't remember at the moment.
+*/
+
 Vue.use(Vuesax)
