b7c501a53e6a44a9b62b025ae5e435ad1d0db38b
NoahCardoza <noahcardoza@gmail.com>
2020-10-07 05:43:43 -0700

    linted vuesax

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Vuesax from 'vuesax'
-import 'vuesax/dist/vuesax.css'
-import 'material-icons/iconfont/material-icons.css'
+import Vue from 'vue';
+import Vuesax from 'vuesax';
+import 'vuesax/dist/vuesax.css';
+import 'material-icons/iconfont/material-icons.css';
 
 /* 
   TODO: while global imports are fine for a hackathon, we should make sure
@@ -9,4 +9,4 @@ import 'material-icons/iconfont/material-icons.css'
   to configure webpack to do this but I can't remember at the moment.
 */
 
-Vue.use(Vuesax)
+Vue.use(Vuesax);
