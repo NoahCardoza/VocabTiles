b49e5aaaf9b8b39b004694bb5b1363db40e2c1b5
NoahCardoza <noahcardoza@gmail.com>
2020-10-07 07:15:04 -0700

    added global app css

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['vuesax/dist/vuesax.css'],
+  css: ['vuesax/dist/vuesax.css', './styles/index.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['@/plugins/vuesax'],
