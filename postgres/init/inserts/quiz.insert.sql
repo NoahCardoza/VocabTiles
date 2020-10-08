2da0558e77e33e1dd80bcd1ce6b7bd131d384280
NoahCardoza <noahcardoza@gmail.com>
2020-10-07 17:21:47 -0700

    restructured db init files

---
+++ postgres/init/inserts/quiz.insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO quiz (user_id, actual_score, max_score) VALUES(1337, 28, 30);
+INSERT INTO quiz (user_id, actual_score, max_score) VALUES(1337, 5, 30);
+INSERT INTO quiz (user_id, actual_score, max_score) VALUES(1337, 15, 15);
