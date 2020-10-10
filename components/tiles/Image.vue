f141200a2830a0ebd4af6ac42bc13b4cfa00c650
NoahCardoza <noahcardoza@gmail.com>
2020-10-09 18:28:50 -0700

    created ImageTile component

---
+++ components/tiles/Image.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-center items-center w-100 h-100">
+    <img :src="image" style="max-width: 100%" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VtImage',
+  components: {},
+  props: {
+    image: {
+      required: true,
+      type: String,
+    },
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {},
+};
+</script>
