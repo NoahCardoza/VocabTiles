ebc189d2c2af86140568d50deb25e275fe202820
NoahCardoza <noahcardoza@gmail.com>
2020-10-09 22:32:35 -0700

    add axios intercepter to attach auth token to api requests

---
+++ plugins/firebase-auth.js
@@ -0,0 +1,19 @@
+const shouldAttachToken = ({ baseURL, url, headers }) =>
+  process.server ||
+  (location.origin === baseURL &&
+    url.startsWith('/api') &&
+    headers != null &&
+    headers['X-Requested-With'] == null);
+
+export default ({ $axios, $fireAuth }) => {
+  $axios.onRequest(async (config) => {
+    if (shouldAttachToken(config)) {
+      config.headers = {
+        'X-Requested-With': 'XMLHttpRequest',
+        Authorization: `Bearer ${await $fireAuth.currentUser.getIdToken()}`,
+        ...config.headers,
+      };
+    }
+    return config;
+  });
+};
