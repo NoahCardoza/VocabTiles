10f624a4217aa46e49845dcf4e3757b716da6dd5
NoahCardoza <noahcardoza@gmail.com>
2020-10-10 07:54:50 -0700

    created function to insert a whole quiz into the DB

---
+++ api/db/quiz.js
@@ -0,0 +1,28 @@
+const pool = require('./index');
+
+const insertAnswer = (quizId, correct) => ({ category, text }) =>
+  pool.query(
+    'INSERT INTO "Answer" (quiz_id, category, text, correct) VALUES($1, $2, $3, $4)',
+    [quizId, category, text, correct]
+  );
+
+const insertNewQuiz = async (
+  uid,
+  { mode, answers: { correct, incorrect } }
+) => {
+  const {
+    rows: [quiz],
+  } = await pool.query(
+    'INSERT INTO "Quiz" (user_id, mode) VALUES($1, $2) RETURNING id',
+    [uid, mode]
+  );
+
+  await Promise.all([
+    Promise.all(correct.map(insertAnswer(quiz.id, true))),
+    Promise.all(incorrect.map(insertAnswer(quiz.id, false))),
+  ]);
+};
+
+module.exports = {
+  insertNewQuiz,
+};
