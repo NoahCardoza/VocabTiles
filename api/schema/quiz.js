d8acbdbd58b47c8bc414c2b8cb7d694c866c50fc
NoahCardoza <noahcardoza@gmail.com>
2020-10-10 06:41:42 -0700

    create QuizSchema definition

---
+++ api/schema/quiz.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const QuestionSchema = Joi.object({
+  text: Joi.string().max(100),
+  category: Joi.string().max(30),
+});
+
+const QuestionsSchema = Joi.array().items(QuestionSchema);
+
+const QuizSchema = Joi.object({
+  // TODO: sould max be dynaically looked up from the config file?
+  mode: Joi.number().integer().min(1).max(4),
+  answers: Joi.object({
+    correct: QuestionsSchema,
+    incorrect: QuestionsSchema,
+  }),
+});
+
+module.exports = {
+  QuestionSchema,
+  QuestionsSchema,
+  QuizSchema,
+};
