165d6eb72f75278a0eab9d507f925cd46ee37822
NoahCardoza <noahcardoza@gmail.com>
2020-10-10 18:08:38 -0700

    created script to merge Quizlet audio into quizzes.json

---
+++ bin/merge-quizlet-audio.py
@@ -0,0 +1,28 @@
+"""
+    TODO: automate uploading questions to Quizlet and scraping data
+    
+    For now:
+        bash:
+            jq '.quizzes[].tiles[].text' -r quizzes.json | pbcopy
+        js:
+            const questions = Quizlet.setPageData.termIdToTermsMap
+            const keys = Object.keys(questions)
+            const data = keys
+                .map(k => ({[questions[k].word]: location.origin + questions[k]._wordAudioUrl}))
+                .reduce((collection, object) => ({...collection, ...object}), {})
+            JSON.stringify(data, null, 2)
+    
+    Save to `speech.json` and run `python3.7 merge-quizlet-audio.py`.
+"""
+
+import json
+
+audio = json.load(open('./speech.json', 'r'))
+parent = json.load(open('../content/quizzes.json', 'r'))
+quizzes = parent['quizzes']
+
+for quiz in quizzes:
+    for tile in quiz['tiles']:
+        tile['ql_audio'] = audio[tile['text']]
+
+json.dump(parent, open('../content/quizzes.json', 'w'), indent=2)
