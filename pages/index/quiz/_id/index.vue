582b4178c6cc278711dc9e0bbd942428d62b56d1
NoahCardoza <noahcardoza@gmail.com>
2020-10-11 07:39:41 -0700

    create quiz results page

---
+++ pages/index/quiz/_id/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Quiz Results</div>
+</template>
+
+<script>
+export default {
+  name: 'QuizResults',
+  components: {},
+  methods: {},
+};
+</script>
+
+<style lang="css" scoped></style>
