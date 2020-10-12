c9d28e2122b592c9c28abad744d1623dd5dfc370
NoahCardoza <noahcardoza@gmail.com>
2020-10-12 06:48:20 -0700

    fixed api endpoint

--- api/db/quiz.js
+++ api/db/quiz.js
@@ -3,7 +3,7 @@ const pool = require('./index');
 // eslint-disable-next-line camelcase
 const insertAnswer = (quizId) => ({ question_id, correct }) =>
   pool.query(
-    'INSERT INTO "Answer" (quiz_id, question_id, correct) VALUES($1, $2, $3) RETURNING id',
+    'INSERT INTO "Answer" (quiz_id, question_id, correct) VALUES($1, $2, $3)',
     // eslint-disable-next-line camelcase
     [quizId, question_id, correct]
   );
@@ -11,10 +11,10 @@ const insertAnswer = (quizId) => ({ question_id, correct }) =>
 const insertNewQuiz = async (uid, { mode, answers }) => {
   const {
     rows: [quiz],
-  } = await pool.query('INSERT INTO "Quiz" (user_id, mode) VALUES($1, $2)', [
-    uid,
-    mode,
-  ]);
+  } = await pool.query(
+    'INSERT INTO "Quiz" (user_id, mode) VALUES($1, $2) RETURNING id',
+    [uid, mode]
+  );
 
   console.log(await Promise.all(answers.map(insertAnswer(quiz.id))));
 
