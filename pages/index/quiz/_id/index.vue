dd6090d8f759a53afcd58e5847a2884a85f2818a
NoahCardoza <noahcardoza@gmail.com>
2020-10-12 01:37:40 -0700

    add table to display questions

--- pages/index/quiz/_id/index.vue
+++ pages/index/quiz/_id/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>Quiz Results</div>
+  <div>
+    <template lang="html">
+      <div>
+        <vs-table :data="quiz.answers">
+          <template slot="header">
+            <h3 class="ma2">Quiz Results</h3>
+          </template>
+          <template slot="thead">
+            <vs-th>Category</vs-th>
+            <vs-th>Question</vs-th>
+            <vs-th>Correct</vs-th>
+            <vs-th>Image</vs-th>
+          </template>
+
+          <template slot-scope="{ data }">
+            <vs-tr v-for="(tr, i) in data" :key="i">
+              <vs-td :data="data[i].category">
+                {{ data[i].category }}
+              </vs-td>
+
+              <vs-td :data="data[i].text">
+                {{ data[i].text }}
+              </vs-td>
+
+              <vs-td :data="data[i].correct">
+                <vs-checkbox
+                  v-model="data[i].correct"
+                  color="success"
+                  @click.native="() => false"
+                ></vs-checkbox>
+              </vs-td>
+              <vs-td :data="data[i].id"></vs-td>
+            </vs-tr>
+          </template>
+        </vs-table>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
