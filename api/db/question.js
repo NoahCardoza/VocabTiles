51899cf0a11c8d86a0916fd4653f1d9c23b01149
NoahCardoza <noahcardoza@gmail.com>
2020-10-12 04:38:28 -0700

    fixed: return row

--- api/db/question.js
+++ api/db/question.js
@@ -1,15 +1,20 @@
 const pool = require('./index');
 
 const getQuestionsByCategoryId = (catId) =>
-  pool.query('SELECT id, audio, text, color, image FROM "Question" WHERE category_id = $1', [catId])
+  pool
+    .query(
+      'SELECT id, audio, text, color, image FROM "Question" WHERE category_id = $1',
+      [catId]
+    )
     .then(({ rows }) =>
       rows.map((row) => {
         if (row.color === null) {
-          delete row['color'];
+          delete row.color;
         }
         if (row.image === null) {
-          delete row['image'];
+          delete row.image;
         }
+        return row;
       })
     );
 
