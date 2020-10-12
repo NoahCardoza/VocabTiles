9806704c81c59832f2b417fe3985c16617dccf7e
NoahCardoza <noahcardoza@gmail.com>
2020-10-12 06:29:00 -0700

    save answers by question_id

--- api/db/quiz.js
+++ api/db/quiz.js
@@ -1,22 +1,22 @@
 const pool = require('./index');
 
 // eslint-disable-next-line camelcase
-const insertAnswer = (quizId) => ({ category_id, correct }) =>
+const insertAnswer = (quizId) => ({ question_id, correct }) =>
   pool.query(
-    'INSERT INTO "Answer" (quiz_id, category_id, correct) VALUES($1, $2, $3)',
+    'INSERT INTO "Answer" (quiz_id, question_id, correct) VALUES($1, $2, $3) RETURNING id',
     // eslint-disable-next-line camelcase
-    [quizId, category_id, correct]
+    [quizId, question_id, correct]
   );
 
 const insertNewQuiz = async (uid, { mode, answers }) => {
   const {
     rows: [quiz],
-  } = await pool.query(
-    'INSERT INTO "Quiz" (user_id, mode) VALUES($1, $2) RETURNING id',
-    [uid, mode]
-  );
+  } = await pool.query('INSERT INTO "Quiz" (user_id, mode) VALUES($1, $2)', [
+    uid,
+    mode,
+  ]);
 
-  await Promise.all(answers.map(insertAnswer(quiz.id)));
+  console.log(await Promise.all(answers.map(insertAnswer(quiz.id))));
 
   return quiz.id;
 };
